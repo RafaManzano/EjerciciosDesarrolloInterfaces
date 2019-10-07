1505209a75082713e7e0a63af917438a50e363e3
RafaManzano <rafamanzanomedina@gmail.com>
2019-10-07 14:31:35 +0200

    Botones

---
+++ 04-BotonesUWP/04-BotonesUWP/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _04_BotonesUWP
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Button boton3;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            boton3.FontFamily;
+                /*> Verdana </ Button.FontFamily >
+            < Button.HorizontalAlignment > Center </ Button.HorizontalAlignment >
+            < Button.VerticalAlignment > Center </ Button.VerticalAlignment >
+            < Button.Height > 70 </ Button.Height >
+            < Button.Width > 200 </ Button.Width >
+            < Button.BorderBrush > Yellow </ Button.BorderBrush >
+            < Button.FontSize > 16 </ Button.FontSize >
+            < Button.FontWeight > Bold </ Button.FontWeight >
+            < Button.Background > Blue </ Button.Background >
+            < Button.Content > Boton 2 </ Button.Content >
+            */
+        }
+
+        
+        
+    }
+}
