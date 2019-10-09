92bd6ec0188b4513dc216a258921532ba466d32f
RafaManzano <rafamanzanomedina@gmail.com>
2019-10-09 14:27:47 +0200

    09-10-2019

--- 05-Layouts/05-Layouts/MainPage.xaml.cs
+++ 05-Layouts/05-Layouts/MainPage.xaml.cs
@@ -15,33 +15,21 @@ using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
 
-namespace _04_BotonesUWP
+namespace _05_Layouts
 {
     /// <summary>
     /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private Button boton3;
         public MainPage()
         {
             this.InitializeComponent();
-            boton3 = new Button();
-            boton3.Content = "Boton 3";
-            boton3.Background = new SolidColorBrush(Windows.UI.Colors.Blue);
-            boton3.FontFamily = new FontFamily("Verdana");
-            boton3.HorizontalAlignment = HorizontalAlignment.Center;
-            boton3.VerticalAlignment = VerticalAlignment.Center;
-            boton3.Height = 70;
-            boton3.Width = 200;
-            boton3.FontSize = 16;
-            boton3.FontWeight = Windows.UI.Text.FontWeights.Bold;
-            boton3.BorderBrush = new SolidColorBrush(Windows.UI.Colors.Yellow);
-
-            panel.Children.Add(boton3);
         }
 
-        
-        
+        private void Btn10_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(VariableSized));
+        }
     }
 }
