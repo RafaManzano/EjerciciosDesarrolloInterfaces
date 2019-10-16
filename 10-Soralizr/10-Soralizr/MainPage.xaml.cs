8a7a500512d2a32f9cf2c00dc892aad075881369
RafaManzano <rafamanzanomedina@gmail.com>
2019-10-16 14:31:30 +0200

    Modificando

--- 10-Soralizr/10-Soralizr/MainPage.xaml.cs
+++ 10-Soralizr/10-Soralizr/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,7 @@ namespace _10_Soralizr
         public MainPage()
         {
             this.InitializeComponent();
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size { Width = 1400, Height = 1400 });
         }
     }
 }
