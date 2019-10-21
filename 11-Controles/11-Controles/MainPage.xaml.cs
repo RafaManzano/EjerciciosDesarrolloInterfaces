ca6f8c1572d9c4148e67ca7f6d99dc49705bdf5f
RafaManzano <rafamanzanomedina@gmail.com>
2019-10-21 14:29:50 +0200

    Mitad

---
+++ 11-Controles/11-Controles/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _11_Controles
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (rootPivot.SelectedIndex > 0)
+            {
+                // If not at the first item, go back to the previous one.
+                rootPivot.SelectedIndex -= 1;
+            }
+            else
+            {
+                // The first PivotItem is selected, so loop around to the last item.
+                rootPivot.SelectedIndex = rootPivot.Items.Count - 1;
+            }
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (rootPivot.SelectedIndex < rootPivot.Items.Count - 1)
+            {
+                // If not at the last item, go to the next one.
+                rootPivot.SelectedIndex += 1;
+            }
+            else
+            {
+                // The last PivotItem is selected, so loop around to the first item.
+                rootPivot.SelectedIndex = 0;
+            }
+        }
+    }
+}
