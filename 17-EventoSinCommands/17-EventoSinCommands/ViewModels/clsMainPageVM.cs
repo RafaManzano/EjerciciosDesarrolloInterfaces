92d6bcb166fc939f953d801f3170e28239fcc7ae
Rafael Manzano Medina <rafamanzanomedina@gmail.com>
2019-11-06 21:26:39 +0100

    Terminado

---
+++ 17-EventoSinCommands/17-EventoSinCommands/ViewModels/clsMainPageVM.cs
@@ -0,0 +1,59 @@
+﻿using _17_EventoSinCommands.Models;
+using _17_EventoSinCommands.Utiles;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace _17_EventoSinCommands.ViewModels
+{
+    public class clsMainPageVM
+    {
+        private clsPersona personaSeleccionada;
+        private ObservableCollection<clsPersona> listadoPersona;
+
+        public clsMainPageVM()
+        {
+            this.listadoPersona = clsListadosPersonas.listadoPersonas();
+        }
+
+        public clsPersona PersonaSeleccionada
+        {
+            get
+            {
+                return personaSeleccionada;
+            }
+            set
+            {
+                this.personaSeleccionada = value;
+            }
+        }
+
+
+        public ObservableCollection<clsPersona> ListadoPersona
+        {
+            get
+            {
+                return listadoPersona;
+            }
+            set
+            {
+                listadoPersona = value;
+            }
+
+        }
+
+        public void Eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            listadoPersona.Remove(personaSeleccionada);
+        }
+
+
+    }
+}
+
