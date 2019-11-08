fbd813037de42a0c5db1f7fb9b6643eafdca5a8d
RafaManzano <rafamanzanomedina@gmail.com>
2019-11-08 14:28:54 +0100

    A la mitad

---
+++ 18-EventoCommands/18-EventoCommands/Utilities/clsListadosPersonas.cs
@@ -0,0 +1,24 @@
+﻿using _18_EventoCommands.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_EventoCommands.Utilities
+{
+    public class clsListadosPersonas
+    {
+        public static ObservableCollection<clsPersona> listadoPersonas()
+        {
+            ObservableCollection<clsPersona> listado = new ObservableCollection<clsPersona>();
+            listado.Add(new clsPersona("Rafael Manuel", "Manzano", "Medina", DateTime.Now));
+            listado.Add(new clsPersona("Victor Manuel", "Perez", "Lobato", DateTime.Now));
+            listado.Add(new clsPersona("Yeray Manuel", "Campanario", "Fernandez", DateTime.Now));
+            listado.Add(new clsPersona("Pablo Manuel", "Prats", "Jimenez", DateTime.Now));
+            listado.Add(new clsPersona("Daniel Manuel", "Gordillo", "Rodriguez", DateTime.Now));
+            return listado;
+        }
+    }
+}
---
+++ 18-EventoCommands/18-EventoCommands/ViewModels/clsMainPageVM.cs
@@ -0,0 +1,65 @@
+﻿using _18_EventoCommands.Models;
+using _18_EventoCommands.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace _18_EventoCommands.ViewModels
+{
+    public class clsMainPageVM
+    {
+        private clsPersona personaSeleccionada;
+        private ObservableCollection<clsPersona> listadoPersona;
+        private DelegateCommand eliminarComando;
+
+        public clsMainPageVM()
+        {
+            this.listadoPersona = clsListadosPersonas.listadoPersonas();
+            //eliminarComando = new DelegateCommand(Eliminar);
+        }
+
+        public clsPersona PersonaSeleccionada
+        {
+            get
+            {
+                return personaSeleccionada;
+            }
+            set
+            {
+                this.personaSeleccionada = value;
+            }
+        }
+
+
+        public ObservableCollection<clsPersona> ListadoPersona
+        {
+            get
+            {
+                return listadoPersona;
+            }
+            set
+            {
+                listadoPersona = value;
+            }
+
+        }
+
+        public DelegateCommand EliminarComando
+        {
+            get
+            {
+                //return eliminarComando = new DelegateCommand(new Action(Eliminar()));
+            }
+        }
+
+        public void Eliminar()
+        {
+            listadoPersona.Remove(personaSeleccionada);
+        }
+
+    }
+}
