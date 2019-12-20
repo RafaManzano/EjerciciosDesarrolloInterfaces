41cd4aba3af112d8537a84cafb79f5588d98e29f
RafaManzano <rafamanzanomedina@gmail.com>
2019-12-20 18:06:33 +0100

    En teoria, esta acabado
    No se puede probar porque el servidor no esta operativo

---
+++ 26-TiroAlPato/26-TiroAlPato-Server/HubPato.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace _26_TiroAlPato_Server
+{
+    public class HubPato : Hub
+    {
+        private int puntuacionJ1;
+        private int puntuacionJ2;
+        private String j1;
+        private string j2;
+        public void EnviarPuntuaciones()
+        {
+            Clients.All.broadcastMessage(puntuacionJ1, puntuacionJ2);
+        }
+
+        public void recogerPuntuacion(int puntuacion)
+        {
+            if(Context.ConnectionId == j1) {
+                puntuacionJ1 += puntuacion;
+            }
+            else
+            {
+                puntuacionJ2 += puntuacion;
+            }
+
+            EnviarPuntuaciones();
+        }
+
+        public void elegirJugador()
+        {
+            if (j1.Equals(""))
+            {
+                j1 = Context.ConnectionId;
+            }
+            else
+            {
+                j2 = Context.ConnectionId;
+            }
+        }
+    }
+}
---
+++ 26-TiroAlPato/26-TiroAlPato-UI/ViewModel/ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace _26_TiroAlPato_UI.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void NotifyPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Propiedades privadas
+        private int tuPuntuacion;
+        private int puntuacionRival;
+        private HubConnection conn;
+        private IHubProxy proxy;
+
+        #endregion
+
+        #region Constructores
+        public ViewModel()
+        {
+            //TODO try Catch
+            conn = new HubConnection("https://chatsignalrderafa.azurewebsites.net");
+            proxy = conn.CreateHubProxy("PatoHub");
+            conn.Start();
+            
+
+            //proxy.On<int, int>("broadcastMessage", tuPuntuacion, puntuacionRival);
+        }
+        #endregion
+
+        #region Metodos
+        private void enviarPuntuacion(int puntuacion)
+        {
+            tuPuntuacion = puntuacion;
+           
+            proxy.Invoke("recogerPuntuacion", tuPuntuacion);
+            
+            //puntuacionRival = j2;
+            //listaMensajes.Add(mensaje);
+        }
+        #endregion
+
+    }
+}
