30515edb5628ce0954575bf6eb85101c8f016b2f
Rafael Manzano Medina <rafamanzanomedina@gmail.com>
2020-01-13 23:06:26 +0100

    Ejercicio diferencias

---
+++ 28-Diferencias/28-Diferencias-UI/ViewModel/MainPageVM.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Shapes;
+
+namespace _28_Diferencias_UI.ViewModel
+{
+    public class MainPageVM : INotifyPropertyChanged
+    {
+        #region Propiedades Privadas
+        private int contador;
+        #endregion
+
+        #region Constructor
+        public MainPageVM()
+        {
+            contador = 0;
+        }
+        #endregion
+
+        #region PropertyChangedEventHandler
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void NotifyPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Get y Set
+
+        public int Contador
+        {
+            get
+            {
+                return contador;
+            }
+
+            set
+            {
+                contador = value;
+                NotifyPropertyChanged("Contador");
+                if (contador == 7)
+                {
+                    FinalPartida();
+                }
+            }
+        }
+        #endregion
+
+        #region Metodo para finalizar juego
+        private async void FinalPartida()
+        {
+            ContentDialog finalPartida = new ContentDialog
+            {
+                Title = "Enhorabuena has completado el desafio",
+                PrimaryButtonText = "Genial!"
+            };
+
+            ContentDialogResult result = await finalPartida.ShowAsync();
+            #endregion
+
+
+        }
+    }
+}
---
+++ 28-Diferencias/28-Diferencias-UI/Views/MainPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using _28_Diferencias_UI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _28_Diferencias_UI
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPageVM vm { get; set; }
+        public MainPage()
+        {
+            this.InitializeComponent();
+            vm = (MainPageVM)this.DataContext;
+        }
+
+        private void Imagenes_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Ellipse elipse = (Ellipse)sender;
+            switch (elipse.Name)
+            {
+                case "d1i":
+                    if(elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d1d.Opacity = 100;
+                        vm.Contador++;
+                    }
+                   
+                    break;
+                case "d1d":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d1i.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+
+                case "d2i":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d2d.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+                case "d2d":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d2i.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+
+                case "d3i":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d3d.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+                case "d3d":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d3i.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+
+                case "d4i":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d4d.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+                case "d4d":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d4i.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+
+                case "d5i":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d5d.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+                case "d5d":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d5i.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+
+                case "d6i":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d6d.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+                case "d6d":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d6i.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+
+                case "d7i":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d7d.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+                case "d7d":
+                    if (elipse.Opacity == 0)
+                    {
+                        elipse.Opacity = 100;
+                        d7i.Opacity = 100;
+                        vm.Contador++;
+                    }
+                    break;
+            }
+        }
+    }
+}
