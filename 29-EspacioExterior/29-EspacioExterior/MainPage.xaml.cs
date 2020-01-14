6dbe86263941fcbbc85c7d97b9782f512a76c92f
RafaManzano <rafamanzanomedina@gmail.com>
2020-01-14 22:00:05 +0100

    Nada
    Queda todavia casi entero

---
+++ 29-EspacioExterior/29-EspacioExterior/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _29_EspacioExterior
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Ellipse e = new Ellipse();
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+            mySolidColorBrush.Color = Color.FromArgb(255, 0, 0, 128);
+            e.Fill = mySolidColorBrush;
+            e.Width = 30;
+            e.Height = 30;
+            canvas.Children.Add(e);
+        }
+    }
+}
